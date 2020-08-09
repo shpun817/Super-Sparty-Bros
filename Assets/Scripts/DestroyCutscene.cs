aa1abd13f434ff20e39d0db6eaca03e68eed2f2e
shpun817 <shpun@connect.ust.hk>
2020-08-09 18:28:49 +0800

    finished

---
+++ Assets/Scripts/DestroyCutscene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCutscene : MonoBehaviour {
+
+    public void DestroyCutsceneObject() {
+		Destroy(transform.parent.gameObject);
+	}
+
+}
